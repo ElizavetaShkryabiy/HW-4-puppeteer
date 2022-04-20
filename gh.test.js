98baf5a1b03ea55a41a03b200eb7109c76501eaf
unknown <evshkryabiy@google.com>
2022-04-20 15:44:30 +0300

    timeouts and 3 tests added

--- gh.test.js
+++ gh.test.js
@@ -1,19 +1,16 @@
-let page;
 
+let page;
+afterEach(() => {
+  page.close();
+});
 
 describe("Github page tests", () => {
-  beforeEach(async () => {
-    page = await browser.newPage(
-      {
-        testTimeout: 60000
-      }
-    );
-    await page.goto("https://github.com/team");
-  });
+  beforeEach(async () => {    
+    page = await browser.newPage();
+    await page.setDefaultNavigationTimeout(60000);
+    await page.goto('https://github.com/team', {timeout: 60000});
+  });  
   
-  afterEach(() => {
-    page.close();
-  });
   test("The h1 header content'", async () => {
     const firstLink = await page.$("header div div a");
     await firstLink.click();
@@ -36,3 +33,27 @@ describe("Github page tests", () => {
     expect(actual).toContain("Sign up for free")
   });
 });
+test("The page featurers title", async () => {
+  page = await browser.newPage();
+  await page.setDefaultNavigationTimeout(60000);
+  await page.goto("https://github.com/features");
+  await page.waitForSelector('h1');
+  const title2 = await page.title();
+  expect(title2).toEqual('Features | GitHub · GitHub');
+})
+test("The page enterprise title", async () => {
+  page = await browser.newPage();
+  await page.setDefaultNavigationTimeout(60000);
+  await page.goto("https://github.com/enterprise");
+  await page.waitForSelector('h1');
+  const title2 = await page.title();
+  expect(title2).toEqual('Enterprise · A smarter way to work together · GitHub');
+})
+test("The page pricing title", async () => {
+  page = await browser.newPage();
+  await page.setDefaultNavigationTimeout(60000);
+  await page.goto("https://github.com/pricing");
+  await page.waitForSelector('h1');
+  const title2 = await page.title();
+  expect(title2).toEqual('Pricing · Plans for every developer · GitHub');
+})
